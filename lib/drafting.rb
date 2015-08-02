8a8bc8f62fab8728b39bcf3d2daa5d5275d6b1ab
Georg Ledermann <mail@georg-ledermann.de>
2015-08-02 13:21:35 +0200

    Simplify extending ActiveRecord

--- lib/drafting.rb
+++ lib/drafting.rb
@@ -6,12 +6,4 @@ require "drafting/class_methods"
 require "drafting/instance_methods"
 require "drafting/draft"
 
-module Drafting
-  def self.included base
-    base.class_exec do
-      extend BaseClassMethods
-    end
-  end
-end
-
-ActiveRecord::Base.send :include, Drafting
+ActiveRecord::Base.extend Drafting::BaseClassMethods
