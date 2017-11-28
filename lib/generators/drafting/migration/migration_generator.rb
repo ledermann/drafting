51600d78d695ef15d3e9217c1aa9470a04f164c7
Georg Ledermann <mail@georg-ledermann.de>
2017-11-28 04:28:30 +0100

    :bug: Migration: Fix missing file extension

--- lib/generators/drafting/migration/migration_generator.rb
+++ lib/generators/drafting/migration/migration_generator.rb
@@ -9,7 +9,7 @@ module Drafting
     source_root File.expand_path('../templates', __FILE__)
 
     def create_migration_file
-      migration_template 'migration.rb', 'db/migrate/drafting_migration'
+      migration_template 'migration.rb', 'db/migrate/drafting_migration.rb'
     end
 
     def self.next_migration_number(dirname)
